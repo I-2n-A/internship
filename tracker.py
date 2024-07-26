3e1bc3204b5a25c9ee72004ae994f7a05b3f7c34
I-2n-A <130758450+I-2n-A@users.noreply.github.com>
2024-07-26 11:51:14 +0300

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -17,6 +17,7 @@ class Tracker:
         sift = cv2.SIFT_create()
         gray_cut = cv2.cvtColor(gray_cut, cv2.COLOR_BGR2GRAY)
         keypoints_1, descriptors_1 = sift.detectAndCompute(gray_cut, None)
+        # в случае первого вхождения присваиваем множеству сохраняемых id и дескрипторов первое значение
         if len(self.id_deckriptor) == 0:
             self.deskriptors.append(descriptors_1)
             self.id_deckriptor.append(self.id_count)
@@ -25,6 +26,10 @@ class Tracker:
         index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
         search_params = dict(checks=50)
         flann = cv2.FlannBasedMatcher(index_params, search_params)
+        # сравнение сохраненных ключевых точек с новыми
+        # ищем наибольшее количество совпадений с имеющимся набором, в self.id_deckriptor сохраняем id под которым был данный набор точек
+        # если действующий (основной) счетчик id больше найденного, значи этот человек уже встречался и был на кадре, то есть
+        # происходит откат основного id до найденного значения
         try:
             mach = []
             for desk in self.deskriptors:
@@ -53,7 +58,7 @@ class Tracker:
             same_object_detected = False
             for id, pt in self.center_points.items():
                 dist = math.hypot(cx - pt[0], cy - pt[1])
-                print(self.id_deckriptor[len(self.id_deckriptor)-1]-1 == id)
+                # print(self.id_deckriptor[len(self.id_deckriptor)-1]-1 == id)
                 if (dist < 100) and (self.id_deckriptor[len(self.id_deckriptor)-1]-1 == id):
                     self.center_points[id] = (cx, cy)
                     objects_bbs_ids.append([x, y, w, h, id])
@@ -65,6 +70,7 @@ class Tracker:
                 self.id_count += 1
                 self.id_deckriptor[len(self.id_deckriptor)-1] = self.id_count
         new_center_points = {}
+        # обновление центра id
         for obj_bb_id in objects_bbs_ids:
             _, _, _, _, object_id = obj_bb_id
             center = self.center_points[object_id]
