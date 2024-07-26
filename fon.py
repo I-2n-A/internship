e11e8a2ec4420a7c951cca77fc842185bf10ab1c
Inna Olhovskaya <21kf.olhovskaya.i@pdu.by>
2024-07-26 11:36:59 +0300

    new start

--- fon.py
+++ fon.py
@@ -1,42 +1,52 @@
 import cv2
 import numpy as np
 from tracker import *
+from threading import Thread
 
 
-cap = cv2.VideoCapture(
+def tracking(cap, fon, nom):
+    tracker = Tracker()
+    while True:
+        ret, frame = cap.read()
+    # Вычитание фона(кадр без людей) из кадра
+        copy = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        dframe = cv2.absdiff(copy, fon)
+        th, dframe = cv2.threshold(dframe, 35, 255, cv2.THRESH_BINARY)
+    # Обнаружение контуров человека
+        _, thresh = cv2.threshold(dframe, 180, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(
+            thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        detections = []
+        if len(contours) != 0:
+            largest_contour = max(contours, key=cv2.contourArea)
+            if cv2.contourArea(largest_contour) > 100:
+                x, y, w, h = cv2.boundingRect(largest_contour)
+                detections.append([x, y, w, h])
+
+        frame_copy = frame.copy()  # Копируем текущий кадр
+        ids_boxes = tracker.update(detections, frame_copy)
+    # Изображение рамки и (id)
+        for ids in ids_boxes:
+            x, y, w, h, id = ids
+            cv2.putText(frame, str(id), (x, y - 15),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 3)
+        cv2.imshow('Camera'+str(nom), frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+fon_4 = cv2.imread('fon4.png')
+fon_4 = cv2.cvtColor(fon_4, cv2.COLOR_BGR2GRAY)
+fon_3 = cv2.imread('fon.png')
+fon_3 = cv2.cvtColor(fon_3, cv2.COLOR_BGR2GRAY)
+camera1 = cv2.VideoCapture(
+    "C:/Users/user/Desktop/st/3.Camera 2017-05-29 16-23-04_137 [3m3s].avi")
+camera2 = cv2.VideoCapture(
     "C:/Users/user/Desktop/st/4.Camera 2017-05-29 16-23-04_137 [3m3s].avi")
-fon = cv2.imread('fon4.png')
-fon = cv2.cvtColor(fon, cv2.COLOR_BGR2GRAY)
-id = 0
-last_keypoint = []
-last_diskriptor = []
-tracker = Tracker()
-while True:
-    ret, frame = cap.read()
-# Вычитание фона(кадр без людей) из кадра
-    copy = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    dframe = cv2.absdiff(copy, fon)
-    th, dframe = cv2.threshold(dframe, 35, 255, cv2.THRESH_BINARY)
-# Обнаружение контуров человека
-    _, thresh = cv2.threshold(dframe, 180, 255, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(
-        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    detections = []
-    if len(contours) != 0:
-        largest_contour = max(contours, key=cv2.contourArea)
-        if cv2.contourArea(largest_contour) > 100:
-            x, y, w, h = cv2.boundingRect(largest_contour)
-            detections.append([x, y, w, h])
-
-    frame_copy = frame.copy()  # Копируем текущий кадр
-    ids_boxes = tracker.update(detections, frame_copy)
-# Изображение рамки и (id)
-    for ids in ids_boxes:
-        x, y, w, h, id = ids
-        cv2.putText(frame, str(id), (x, y - 15),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 3)
-    cv2.imshow('Camera-1', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+Cam1 = Thread(target=tracking, args=(camera1, fon_3, 3))
+Cam2 = Thread(target=tracking, args=(camera2, fon_4, 4))
+Cam1.start()
+Cam2.start()
+cv2.destroyAllWindows()
--- tracker.py
+++ tracker.py
@@ -10,24 +10,34 @@ class Tracker:
         self.center_points = {}
         self.id_count = 0
         self.distance_threshold = distance_threshold
+        self.id_deckriptor = []
         self.deskriptors = []
 
     def match(self, gray_cut):
         sift = cv2.SIFT_create()
         gray_cut = cv2.cvtColor(gray_cut, cv2.COLOR_BGR2GRAY)
         keypoints_1, descriptors_1 = sift.detectAndCompute(gray_cut, None)
-        self.deskriptors.append(descriptors_1)
-        # print(len(self.deskriptors))
-        MIN_MATCH_COUNT = 5
+        if len(self.id_deckriptor) == 0:
+            self.deskriptors.append(descriptors_1)
+            self.id_deckriptor.append(self.id_count)
+
         FLANN_INDEX_KDTREE = 0
         index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
         search_params = dict(checks=50)
         flann = cv2.FlannBasedMatcher(index_params, search_params)
         try:
-            matches = flann.knnMatch(
-                descriptors_1, self.deskriptors[len(self.deskriptors)-1], k=2)
-            print(len(matches))
-            return matches
+            mach = []
+            for desk in self.deskriptors:
+                matches = flann.knnMatch(
+                    descriptors_1, desk, k=2)
+                mach.append(matches)
+            max_value = max(mach)
+            max_index = mach.index(max_value)
+            self.id_deckriptor.append(self.id_deckriptor[max_index])
+            self.deskriptors.append(descriptors_1)
+            if self.id_deckriptor[len(self.id_deckriptor)-1] < self.id_count:
+                self.id_count = self.id_deckriptor[len(self.id_deckriptor)-1]
+            # return matches
         except:
             return 0
 
@@ -36,14 +46,15 @@ class Tracker:
         for rect in objects_rect:
             x, y, w, h = rect
             cut = frame[y:y+h, x:x+w]
-            cv2.imshow('3', cut)
+            # cv2.imshow('3', cut)
             sopostavlenie = self.match(cut)
             cx = (x + x + w) // 2
             cy = (y + y + h) // 2
             same_object_detected = False
             for id, pt in self.center_points.items():
                 dist = math.hypot(cx - pt[0], cy - pt[1])
-                if (dist < 100) or (sopostavlenie != 0):
+                print(self.id_deckriptor[len(self.id_deckriptor)-1]-1 == id)
+                if (dist < 100) and (self.id_deckriptor[len(self.id_deckriptor)-1]-1 == id):
                     self.center_points[id] = (cx, cy)
                     objects_bbs_ids.append([x, y, w, h, id])
                     same_object_detected = True
@@ -52,6 +63,7 @@ class Tracker:
                 self.center_points[self.id_count] = (cx, cy)
                 objects_bbs_ids.append([x, y, w, h, self.id_count])
                 self.id_count += 1
+                self.id_deckriptor[len(self.id_deckriptor)-1] = self.id_count
         new_center_points = {}
         for obj_bb_id in objects_bbs_ids:
             _, _, _, _, object_id = obj_bb_id
